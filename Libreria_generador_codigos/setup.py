2764dea0b14423b0a9b28c03e99832e68094605d
Sara Crego <sara.crego@alumni.mondragon.edu>
2023-11-11 13:27:56 +0100

    Modificaciones y correciones de errores

--- Libreria_generador_codigos/setup.py
+++ Libreria_generador_codigos/setup.py
@@ -17,7 +17,7 @@ setup(
     author='Sara & Nahia',
     author_email='sara.crego@alumni.mondragon.edu',
     description='Libreria diseñada para la generación de QRs para paginas web, especialmente e-commerce, donde además tiene la posibilidad de generar códigos de barras para cada uno de los distintos productos que están a la venta en la tienda.',
-    long_description=open('README.rst').read(),  # Ajusta el nombre del archivo según tu caso
+    long_description=open('README.md').read(),  # Ajusta el nombre del archivo según tu caso
     long_description_content_type='text/markdown',
     url='https://github.com/nahiaunceta/GuardarCodigo_Progra_bda4_Sara_Nahia.git',
     classifiers=[
