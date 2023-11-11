b88f4733e93291f8b9289d090c9b9a989d325f1d
Sara Crego <sara.crego@alumni.mondragon.edu>
2023-11-11 15:04:15 +0100

    Actualización final

--- GeneradorCodigos_CodBarras_QRs/setup.py
+++ GeneradorCodigos_CodBarras_QRs/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Generador_Codigos_QR_CodBarras',
+    name='GeneradorCodigos_CodBarras_QRs',
     version='0.4',
     packages=find_packages(),
     install_requires=[
@@ -11,18 +11,17 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'trabajo_grupal.py=Generador_Codigos_QR_CodBarras.trabajo_grupal.py:main',
+            'trabajo_grupal.py=GeneradorCodigos_CodBarras_QRs.trabajo_grupal.py:main',
         ],
     },
     author='Sara & Nahia',
     author_email='sara.crego@alumni.mondragon.edu',
-    description='Libreria diseñada para la generación de QRs para páginas web, especialmente e-commerce, donde además tiene la posibilidad de generar códigos de barras para cada uno de los distintos productos que están a la venta en la tienda.',
+    description='Libreria diseñada para la creación de QRs para tiendas con páginas web, especialmente eCommerce, donde además tiene la posibilidad de generar códigos de barras para cada uno de los distintos productos que están a la venta en la tienda.',
     long_description=open('../../GuardarCodigo_Progra_bda4_Sara_Nahia/README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/nahiaunceta/GuardarCodigo_Progra_bda4_Sara_Nahia.git',
     classifiers=[
         'Programming Language :: Python :: 3',
-        # Otros clasificadores...
     ]
 
 )
