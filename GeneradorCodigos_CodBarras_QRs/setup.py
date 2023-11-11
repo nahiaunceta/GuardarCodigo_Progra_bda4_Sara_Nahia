ce002eef4c043533d0f369162574ec0b0ece7b24
Sara Crego <sara.crego@alumni.mondragon.edu>
2023-11-11 17:37:41 +0100

    Actualizaciones de mejora

--- GeneradorCodigos_CodBarras_QRs/setup.py
+++ GeneradorCodigos_CodBarras_QRs/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GeneradorCodigos_CodBarras_QRs',
-    version='0.4.1',
+    version='0.4.2',
     license='MIT', 
     packages=find_packages(),
     install_requires=[
